7035dfb87d5bc6ba74d1f7420b6b83df469c516d
divyaparmar1 <112643573+divyaparmar1@users.noreply.github.com>
2023-08-02 14:35:29 +0530

    password_strength_checker.py

--- password strength checker.py
+++ password strength checker.py
@@ -5,32 +5,38 @@ def check_password_strength():
     strength = 0
 
     # Check for length and complexity
-    if len(password)>=8:
-        strength +=1
+    if len(password) >= 8:
+        strength += 1
     if any(char.isupper() for char in password):
-        strength +=1
+        strength += 1
     if any(char.islower() for char in password):
-        strength +=1 
+        strength += 1 
     if any(char.isdigit() for char in password):
-        strength +=1  
+        strength += 1  
     if any(not char.isalnum() for char in password):
-        strength +=1
+        strength += 1
     
     # Evaluate password strength
     if strength == 0:
         result = "Very Weak"
+        color = "red"
     elif strength == 1:
         result = "Weak"
+        color = "orange"
     elif strength == 2:
         result = "Moderate"
+        color = "yellow"
     elif strength == 3:
         result = "Moderate"
+        color = "yellow"
     elif strength == 4:
         result = "Strong"
+        color = "green"
     else:
         result = "Very Strong"
+        color = "darkgreen"
     
-    label_hash_result.config(text=f"Password strength: {result}")
+    label_hash_result.config(text=f"Password strength: {result}", fg=color)
 
 # Create the main application window
 app = tk.Tk()
@@ -46,7 +52,7 @@ password_entry.pack()
 check_button = tk.Button(app, text="Check", command=check_password_strength)
 check_button.pack()
 
-label_hash_result = tk.Label(app, text="")
+label_hash_result = tk.Label(app, text="", fg="black")
 label_hash_result.pack()
 
 # Start the main event loop
